e182def9f5b8e83094fecf0d77ec72a96bf9b84b
Mikele-Kochas <michalkocher24@gmail.com>
2024-12-20 17:22:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,5 +7,6 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    # Uruchomienie aplikacji na porcie przekazanym przez Heroku
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
